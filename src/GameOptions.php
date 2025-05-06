b873a7bdb976a04223c216968b4d46420ddd0d5a
Mistralys <s.mordziol@gmail.com>
2025-05-06 17:18:26 +0200

    Added new options.

--- src/GameOptions.php
+++ src/GameOptions.php
@@ -1,4 +1,8 @@
 <?php
+/**
+ * @package VortexModExporter
+ * @subpackage Games
+ */
 
 declare(strict_types=1);
 
@@ -6,10 +10,18 @@ namespace Mistralys\VortexModExporter;
 
 use AppUtils\ArrayDataCollection;
 
+/**
+ * Stores game-related options.
+ *
+ * @package VortexModExporter
+ * @subpackage Games
+ */
 class GameOptions extends ArrayDataCollection
 {
     public const KEY_IGNORE_DATE_TAGS = 'ignoreDateTags';
     public const KEY_IGNORE_UNKNOWN_CATEGORY = 'ignoreUnknownCategory';
+    public const KEY_DEF_INCLUDE_UNUSED_MODS = 'includeUnusedMods';
+    public const KEY_DEF_INCLUDE_TEMPORARILY_UNUSED = 'includeTemporarilyUnusedMods';
 
     /**
      * Whether to ignore all tags that are based on a date.
@@ -31,5 +43,15 @@ class GameOptions extends ArrayDataCollection
     public function isUnknownCategoryIgnored() : bool
     {
         return $this->getBool(self::KEY_IGNORE_UNKNOWN_CATEGORY);
+    }
+
+    public function areUnusedModsIncluded() : bool
+    {
+        return $this->getBool(self::KEY_DEF_INCLUDE_UNUSED_MODS);
+    }
+
+    public function areTemporarilyUnusedModsIncluded() : bool
+    {
+        return $this->getBool(self::KEY_DEF_INCLUDE_TEMPORARILY_UNUSED);
     }
 }
